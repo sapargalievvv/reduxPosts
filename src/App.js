e8b80711382626af3036e12256d0fce0382797eb
sapargalievvv <sapargalievbektas071@gmail.com>
2024-03-04 20:36:37 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import PostsList from "./features/posts/PostsList";
+import AddPostFrom from "./features/posts/AddPostFrom";
+
+function App() {
+  return (
+    <main className="App">
+      <AddPostFrom />
+      <PostsList />
+    </main>
+  );
+}
+
+export default App;
